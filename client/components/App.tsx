1e6f880b2add087a6214c316850dfd556ea913f2
James McIlraith <jamesmcilraith@gmail.com>
2024-11-28 16:05:39 +1300

    Update to app and layout

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,14 +1,21 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Layout from './Layout'
+import LandingPage from '../components/LandingPage/LandingPage'
+import PieDetails from '../components/PiePage/PieDetails'
+import StoreInfo from '../components/StorePage/StoreInfo'
 
 const App = () => {
   return (
     <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<BestPie />} />{' '}
-        </Routes>
-      </Layout>
+      <Routes>
+        {/* Define the layout route with an outlet */}
+        <Route path="/" element={<Layout />}>
+          {/* Define the nested routes that will be rendered inside Layout's Outlet */}
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/pie/:id" element={<PieDetails />} />
+          <Route path="/store" element={<StoreInfo />} />
+        </Route>
+      </Routes>
     </Router>
   )
 }
--- client/components/Layout.tsx
+++ client/components/Layout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom'
 import Header from '../components/Header/Header'
 import Footer from '../components/Footer/Footer'
 
-const Layout: React.FunctionComponent = () => {
+const Layout = () => {
   return (
     <div className="layout">
       <Header />
