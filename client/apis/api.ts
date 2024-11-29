8399b462f2549b7051de92f4607c59fa24addfc3
Thomas <funnyguytest@yahoo.com>
2024-11-29 16:24:50 +1300

    leaderboard

--- client/apis/api.ts
+++ client/apis/api.ts
@@ -6,18 +6,31 @@ const baseUrl = '/api/v1/pies'
 
 export async function getPies(): Promise<Pie[]> {
   const result = await request.get(`${baseUrl}`).then((res) => {
-    console.log(res.body)
     return res.body
   })
   return result
 }
 
-export async function getPieByFlavor(flavor: string): Promise<Pie> {
+export async function getPieByFlavor(flavor: string) {
+  console.log(flavor)
   const result = await request
-    .get(`${baseUrl}?flavor=${flavor}`)
+    .get(`${baseUrl}/flavor/${flavor}`)
     .then((res) => {
       console.log(res.body)
-      return res.body.length > 0 ? res.body[0] : null
+      return res.body.length > 0
+        ? { ...res.body[0], img: '/images/pie-cartoon.jpg' }
+        : null
+    })
+  return result
+}
+
+export async function getPiesByFlavor(flavor: string) {
+  console.log(flavor)
+  const result = await request
+    .get(`${baseUrl}/flavor/${flavor}`)
+    .then((res) => {
+      console.log(res.body)
+      return res.body as Pie[]
     })
   return result
 }
