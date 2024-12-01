2202c6d7ff28ae9cc4f3b9b7c8b502f1c9bfd2c1
James McIlraith <jamesmcilraith@gmail.com>
2024-12-02 00:15:21 +1300

    Changes to the App.tsx routing and update to the API

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,23 +1,21 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Layout from './Layout'
-import LandingPage from './LandingPage/LandingPage'
-import PieDetails from './PiePage/PieDetails'
+import BestPies from './LandingPage/BestPies'
 import StoreInfo from './StorePage/StoreInfo'
 import PiesList from './PiePage/PiesList'
 import StoreList from './StorePage/StoreList'
-import SinglePie from './PiePage/SinglePie'
+import OnePie from './PiePage/OnePie'
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Layout />}>
-          <Route path="/" element={<LandingPage />} />
+          <Route path="/" element={<BestPies />} />
           <Route path="/pies/" element={<PiesList />} />
           <Route path="/stores" element={<StoreList />} />
-          <Route path="/pies/:id" element={<SinglePie />} />
-          <Route path="/store/:id" element={<StoreInfo />} />
-          <Route path="/pies/flavor/:flavor" element={<PieDetails />} />
+          <Route path="/pies/:id" element={<OnePie />} />
+          <Route path="/stores/:id" element={<StoreInfo />} />
         </Route>
       </Routes>
     </Router>
