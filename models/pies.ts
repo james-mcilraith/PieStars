edc41ed2fcfe94fa17c01cdddc99565281227ab3
James McIlraith <jamesmcilraith@gmail.com>
2024-12-02 00:13:10 +1300

    Minor changes to the models

--- models/pies.ts
+++ models/pies.ts
@@ -1,11 +1,12 @@
-export interface PieData{
+export interface PieData {
   flavor: string
   place: string
   baker: string
-  bakery:string
-  address:string
+  bakery: string
+  address: string
+  img?: string
 }
 
-export interface Pie extends PieData{
-  id: number 
+export interface Pie extends PieData {
+  id: number
 }
