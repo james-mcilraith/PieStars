5df6299574c2bf64b025e707fd161fe9f0a69931
Thomas <funnyguytest@yahoo.com>
2024-12-04 12:39:38 +1300

    fix

--- server/db/seeds/ratings.js
+++ server/db/seeds/ratings.js
@@ -13,13 +13,3 @@ export const seed = async function (knex) {
     },
   ])
 }
-
-export const seed = async function(knex) {
-  // Deletes ALL existing entries
-  await knex('userratings').del()
-  await knex('userratings').insert([
-    {id: 1, userid: 'rowValue1', flavor:'mince', rating:'', bakery:'', address:''}, 
-    {id: 2, userid: 'rowValue2', flavor:'mince', rating:'', bakery:'', address:''},
-    {id: 3, userid: 'rowValue3', flavor:'', rating:'', bakery:'', address:''}
-  ]);
-};
